4d5c9e03e26773ef68a665e4b4428d94939d981f
solaxds <67832977+solaxds@users.noreply.github.com>
2022-02-17 20:07:06 +0000

    Update and rename config.example.js to config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 // To do this, we create a file at the root of our project called 'config.js'
 
 // nftaddress is = to empty string
-// export const nftaddress = ""
-// export const nftmarketaddress = ""
+export const nftaddress = "0x9Dd4a0E94270DD2D69d2897a757E7E2F9808a593"
+export const nftmarketaddress = "0xf7f6c3b5c6e99C3150054deFF9c7C700F62E37c3"
 
 // Once we've deployed, we'll have those values filled in
 // So now we can return to index.js and import those - so that we have a reference to our addresses
